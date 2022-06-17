cdac66ab12f2a7e2d30f18845e739f984e7b1ccd
Steven Ngov <gl.steven.ngov@gmail.com>
2022-06-17 16:36:08 -0400

    Create Appointment components, props and state in the Appointment Form

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import InterviewerList from '../InterviewerList';
+import Button from '../Button';
+
+export default function Form(props) {
+
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  }
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
+
+  const save = () => {
+    props.onSave(student, interviewer);
+  }
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
+          />
+        </form>
+        <InterviewerList
+          value={interviewer}
+          interviewers={props.interviewers}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={save}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
